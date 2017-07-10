0114f88daf6de68d5c983ee214cd5da3051b7681
Angelina Cole <acole@thoughtworks.com>
2017-07-10 11:50:46 -0500

    housekeeping for the app, initial tests passing

---
+++ README.md
@@ -0,0 +1,6 @@
+##Sparkles: Keeping track of your luminous life ##
+Sparkles is a small React application that allows you to keep a short, daily log of happenings in your life. 
+
+##Built With ##
+React Bare App: [react-bare-app](#https://github.com/ryanoglesby08/react-bare-app) 
+
--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 import InputField from "./InputField/InputField";
+import Button from "./Button/Button";
 
 const App = () => (
-    <div>
-        <header>Get your sparkle on</header>
-        <InputField/>
-        <Button/>
-        <footer>Made with love in Chicago</footer>
-    </div>
+  <div>
+    <header>Get your sparkle on</header>
+    <InputField/>
+    <Button/>
+    <footer>Made with love in Chicago</footer>
+  </div>
 );
 
 export default App;
